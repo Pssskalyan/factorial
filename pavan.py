eb9b801e8d12f0ad31a79f1eb54b289513b569d6
PAVANKALYAN <41368953+Pssskalyan@users.noreply.github.com>
2019-05-24 22:49:09 +0530

    Create pavan.py

---
+++ pavan.py
@@ -0,0 +1,16 @@
+def factorial(num):
+    
+    if num == 1:
+        return num
+    else:
+        return num * factorial(num - 1)
+
+
+num = int(input())
+
+if num < 0:
+    print()
+elif num == 0:
+    print("1")
+else:
+    print(factorial(num))
